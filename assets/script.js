eb4c2684d7c467323b88b42087ef750db1e10bcd
empipio <94937507+empipio@users.noreply.github.com>
2022-01-28 23:10:51 +0000

    icon and uv index now showing correctly

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,14 @@
 
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity,
-//the wind speed, and the UV index
+// the wind speed, and the UV index
 
 // WHEN I view the UV index
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
 
 // WHEN I view future weather conditions for that city
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature,
-//the wind speed, and the humidity
+// the wind speed, and the humidity
 
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
@@ -34,16 +34,17 @@ var searchedCities = [];
 
 //icon/temp/humidity/wind speed/uv index
 
-//brings everything out of LS every time another search occurs!!!
 function renderButtons() {
   var savedCities = JSON.parse(localStorage.getItem("cities"));
-  for (var i = 0; i < savedCities.length; i++) {
-    var listButtonEl = document.getElementById("saved-cities");
-    var nameEl = document.createElement("li");
-    var buttonEl = document.createElement("button");
-    buttonEl.innerText = savedCities[i];
-    nameEl.append(buttonEl);
-    listButtonEl.append(nameEl);
+  if (savedCities !== null) {
+    for (var i = 0; i < savedCities.length; i++) {
+      var listButtonEl = document.getElementById("saved-cities");
+      var nameEl = document.createElement("li");
+      var buttonEl = document.createElement("button");
+      buttonEl.innerText = savedCities[i];
+      nameEl.append(buttonEl);
+      listButtonEl.append(nameEl);
+    }
   }
 }
 
@@ -51,7 +52,9 @@ function saveResults() {
   var searchTerm = searchFormEl.value.trim();
   searchedCities.push(searchTerm);
   localStorage.setItem("cities", JSON.stringify(searchedCities));
-  renderButtons();
+  //brings everything out of LS every time another search occurs when not commented out
+  //when commented out, only brings out what you searched for last time but updated only when refreshed
+  //renderButtons();
 }
 
 function getWeather() {
@@ -89,29 +92,48 @@ function getWeather() {
             cityNameEl.textContent = searchFormEl.value;
             var todaysDateEl = document.getElementById("date");
             todaysDateEl.textContent = moment().format("MMM Do, YYYY");
-            var conditionsArr = [
-              city.current.weather[0].icon,
-              city.current.wind_speed,
-              city.current.humidity,
-              city.current.temp,
-              city.current.uvi,
-              //need to colour code uv index
-            ];
-            console.log(city);
-            var headerArr = [
-              "Today will be: ",
-              "Wind Speed: ",
-              "Humidity: ",
-              "Temperature: ",
-              "UV index: ",
-            ];
-            for (var i = 0; i < headerArr.length; i++) {
-              var conditionEl = document.createElement("li");
-              conditionEl[i] = headerArr[i] + conditionsArr[i];
-              todaysDateEl.append(conditionEl[i]);
+            var divElTwo = document.createElement("div");
+            todaysDateEl.append(divElTwo);
 
-              //for UV: low 0-2, mod 3-6, high 7+. Change text colour and add description?
+            var todayEl = document.createElement("li");
+            var todayLabel = document.createElement("p");
+            todayLabel.innerText = "Today will be: ";
+            todayEl.append(todayLabel);
+            var todayIcon = document.createElement("img");
+            todayIcon.setAttribute(
+              "src",
+              `http://openweathermap.org/img/wn/${city.current.weather[0].icon}@2x.png`
+            );
+            todayEl.append(todayIcon);
+            todaysDateEl.append(todayEl);
+
+            var windEl = document.createElement("li");
+            windEl.innerText =
+              "Wind speed: " + city.current.wind_speed + " MPH";
+            todaysDateEl.append(windEl);
+            var humidityEl = document.createElement("li");
+            humidityEl.innerText = "Humidity: " + city.current.humidity + " %";
+            todaysDateEl.append(humidityEl);
+            var temperatureEl = document.createElement("li");
+            temperatureEl.innerText =
+              "Temperature: " + city.current.temp + " degrees";
+            todaysDateEl.append(temperatureEl);
+
+            var uvContainer = document.createElement("li");
+            var uvBox = document.createElement("div");
+            var uvText = document.createElement("p");
+            uvText.innerText = "UV index: " + city.current.uvi;
+            uvText.setAttribute("style", "color: white");
+            if (city.current.uvi < 3) {
+              uvBox.setAttribute("style", "background-color: green");
+            } else if (city.current.uvi < 7) {
+              uvBox.setAttribute("style", "background-color: orange");
+            } else {
+              uvBox.setAttribute("style", "background-color: red");
             }
+            uvBox.append(uvText);
+            uvContainer.append(uvBox);
+            todaysDateEl.append(uvContainer);
 
             //5 DAY STUFF BELOW NOT YET FINISHED
 
@@ -120,39 +142,41 @@ function getWeather() {
             //date, icon, temperature, windspeed, humidity
             //for loop, make a card for each day and render info to each
             //colour code UV index
-            // var forecastHeaders = [
-            //   "Date: ",
-            //   "The weather will be: ",
-            //   "Temperature: ",
-            //   "Wind Speed: ",
-            //   "Humidity: ",
-            // ];
+            var forecastHeaders = [
+              "Date: ",
+              "The weather will be: ",
+              "Temperature: ",
+              "Wind Speed: ",
+              "Humidity: ",
+            ];
 
-            // for (var i = 1; i < forecastHeaders.length; i++) {
-            //   var forecastArr = [
-            //     city.daily[i].dt,
-            //     city.daily[i].weather[0].icon,
-            //     city.daily[i].temp.day,
-            //     city.daily[i].wind_speed,
-            //     city.daily[i].humidity,
-            //   ];
+            for (var i = 1; i < forecastHeaders.length; i++) {
+              var forecastArr = [
+                city.daily[i].dt,
+                city.daily[i].weather[0].icon,
+                city.daily[i].temp.day,
+                city.daily[i].wind_speed,
+                city.daily[i].humidity,
+              ];
+              //how to put all the info on each card? two loops? nest them?
+              var resultsArea = document.getElementById("5-day-forecast");
+              var resultCard = document.createElement("div");
+              resultCard.classList.add("card");
+              resultsArea.append(resultCard);
 
-            //   var resultsArea = document.getElementById("5-day-forecast");
-            //   var resultCard = document.createElement("div");
-            //   resultCard.classList.add("card");
-            //   resultCard.append(resultsArea);
+              var resultBody = document.createElement("div");
+              resultBody.classList.add("card-body");
+              resultCard.append(resultBody);
+              for (var i = 1; i < forecastHeaders.length; i++) {
+                var titleEl = document.createElement("ul");
+                titleEl.textContent = city.daily[i].dt;
+                resultBody.append(titleEl);
 
-            //   var resultBody = document.createElement("div");
-            //   resultBody.classList.add("card-body");
-            //   resultCard.append(resultBody);
-
-            //   var titleEl = document.createElement("ul");
-            //   titleEl.textContent = city.daily[i].dt;
-            //   resultBody.append(titleEl);
-
-            //   var forecastInfo = document.createElement("li");
-            //   forecastInfo.textContent = forecastHeaders[i] + forecastArr[i];
-            // }
+                var forecastInfo = document.createElement("li");
+                forecastInfo.textContent = forecastHeaders[i] + forecastArr[i];
+                resultBody.append(forecastInfo);
+              }
+            }
           });
       });
   } else {
@@ -163,3 +187,5 @@ function getWeather() {
 
 renderButtons();
 searchButtonEl.addEventListener("click", getWeather);
+
+//put event listeners on buttons for previous cities-set attributes?
